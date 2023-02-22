226aa9d1f8b9660ca17253b05a77cd33d1a8cb39
Andrew Liese <andrew.liese.79@gmail.com>
2023-02-23 07:41:33 +1300

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Do not go gentle into that good night Do not go gentle into that good night, Old age should burn and rave at close of day; Rage, rage against the dying of the light
   <!-- END -->
   
   </div>
