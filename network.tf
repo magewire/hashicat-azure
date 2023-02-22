7db420265a7c0ec49dc882a9b688a897387e8a7d
magewire <andrew.liese@stantec.com>
2023-02-22 20:33:22 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,6 @@
 module "network" {
   source  = "app.terraform.io/dp-apac/network/azurerm"
   version = "3.5.0"
+  resource_group_name = "${var.prefix}-workshop"
   # insert required variables here
 }
